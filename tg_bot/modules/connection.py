7648b2826afa56b151e4804cc76227060c435221
Singh <avatarx0tic@gmail.com>
2020-11-18 12:06:09 +0530

    Update connection.py

--- tg_bot/modules/connection.py
+++ tg_bot/modules/connection.py
@@ -12,11 +12,10 @@ from telegram.utils.helpers import mention_html
 
 import tg_bot.modules.sql.connection_sql as sql
 from tg_bot import dispatcher, LOGGER, SUDO_USERS
-from tg_bot.modules.helper_funcs.alternate import send_message, typing_action
+from tg_bot.modules.helper_funcs.alternate import send_message
 from tg_bot.modules.helper_funcs.chat_status import bot_admin, user_admin, is_user_admin, can_restrict
 from tg_bot.modules.helper_funcs.extraction import extract_user, extract_user_and_text
 from tg_bot.modules.helper_funcs.string_handling import extract_time
-from tg_bot.modules.helper_funcs.alternate import send_message
 
 # from tg_bot.modules.translations.strings import tld
 
