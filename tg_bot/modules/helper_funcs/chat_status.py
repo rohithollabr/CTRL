5589d23a699198373550c3fbf277c987966d23e3
Singh <avatarx0tic@gmail.com>
2020-11-26 10:19:58 +0530

    Update chat_status.py

--- tg_bot/modules/helper_funcs/chat_status.py
+++ tg_bot/modules/helper_funcs/chat_status.py
@@ -171,3 +171,44 @@ def user_not_admin(func):
             return func(bot, update, *args, **kwargs)
 
     return is_not_admin
+
+def user_can_ban(func):
+    @wraps(func)
+    def user_is_banhammer(bot: Bot, update: Update, *args, **kwargs):
+        user = update.effective_user.id
+        member = update.effective_chat.get_member(user)
+        if not (member.can_restrict_members or member.status == "creator") \
+                and user not in SUDO_USERS and user != 1087968824:
+            update.effective_message.reply_text("Sorry son, but you're not worthy to wield the banhammer.")
+            return ""
+        return func(bot, update, *args, **kwargs)
+    
+    return user_is_banhammer
+
+
+def user_can_mute(func):
+    @wraps(func)
+    def user_has_tape(bot: Bot, update: Update, *args, **kwargs):
+        user = update.effective_user.id
+        member = update.effective_chat.get_member(user)
+        if not (member.can_restrict_members or member.status == "creator") \
+                and user not in SUDO_USERS and user != 1087968824:
+            update.effective_message.reply_text("You ran out of tape!")
+            return ""
+        return func(bot, update, *args, **kwargs)
+    
+    return user_has_tape
+    
+    
+def user_can_warn(func):
+    @wraps(func)
+    def user_is_warnhammer(bot: Bot, update: Update, *args, **kwargs):
+        user = update.effective_user.id
+        member = update.effective_chat.get_member(user)
+        if not (member.can_restrict_members or member.status == "creator") \
+                and user not in SUDO_USERS and user != 1087968824:
+            update.effective_message.reply_text("You don't have the necessary permissions!")
+            return ""
+        return func(bot, update, *args, **kwargs)
+    
+    return user_is_warnhammer
