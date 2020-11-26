ba2e8c8f6f6423ab70b2bc1fe72b25abab6a6885
Singh <avatarx0tic@gmail.com>
2020-11-26 17:56:18 +0530

    Update admin.py

--- tg_bot/modules/admin.py
+++ tg_bot/modules/admin.py
@@ -71,7 +71,7 @@ def promote(bot: Bot, update: Update, args: List[str]) -> str:
                           can_pin_messages=bot_member.can_pin_messages,
                           can_promote_members=bot_member.can_promote_members)
 
-    message.reply_text("Promoted")
+    message.reply_text("Successfully promoted {} in {}.".format(mention_html(member.user.id, member.user.first_name), (chat.title)
     return (
         "<b>{}:</b>"
         "\n#PROMOTED"
