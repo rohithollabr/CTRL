2d782805fd6f70fff70171d4de527fa7550c1f32
Singh <avatarx0tic@gmail.com>
2020-11-27 10:36:23 +0530

    Update __init__.py

--- tg_bot/modules/helper_funcs/Tclient/__init__.py
+++ tg_bot/modules/helper_funcs/Tclient/__init__.py
@@ -1,14 +1,14 @@
 from tg_bot import (
     Tclient,
     SUDO_USERS,
-    OWNER_ID
+    WHITELIST_USERS
 )
 
 MOD_USERS = SUDO_USERS + OWNER_ID
 
 MOD_USERS = (
     list(SUDO_USERS)
-    + list(OWNER_ID)
+    + list(WHITELIST_USERS)
 )
 
 MOD_USERS.append(1087968824)
