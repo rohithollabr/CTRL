7be39440e0d652b8d818b0897397c3c44c795776
Singh <avatarx0tic@gmail.com>
2020-11-28 22:04:25 +0530

    Update muting.py

--- tg_bot/modules/muting.py
+++ tg_bot/modules/muting.py
@@ -216,14 +216,14 @@ def nomedia(bot: Bot, update: Update, args: List[str]) -> str:
                                      can_send_media_messages=False,
                                      can_send_other_messages=False,
                                      can_add_web_page_previews=False)
-                message.reply_text("Yep, {} is restricted from sending media in **{}**!".format(mention_html(member.user.id, member.user.first_name), html.escape(chat.title)), parse_mode=ParseMode.HTML)
-                return "<b>{}:</b>" \
+               message.reply_text("Yep, {} is restricted from sending media in **{}**!".format(mention_html(member.user.id, member.user.first_name), html.escape(chat.title)), parse_mode=ParseMode.HTML)
+               return "<b>{}:</b>" \
                    "\n#RESTRICTED" \
                    "\n<b>• Admin:</b> {}" \
                    "\n<b>• User:</b> {}" \
                    "\n<b>• ID:</b> <code>{}</code>".format(html.escape(chat.title),
-                                              mention_html(user.id, user.first_name),
-                                              mention_html(member.user.id, member.user.first_name), user_id)
+                                                           mention_html(user.id, user.first_name),
+                                                           mention_html(member.user.id, member.user.first_name), user_id)
         else:
             message.reply_text("This user is already restricted in **{}**".format(chat.title))
     else:
