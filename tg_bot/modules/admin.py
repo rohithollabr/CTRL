4e1e36f84ba10d9cef51a49b885514dfea0e7f42
Singh <avatarx0tic@gmail.com>
2020-12-02 18:27:25 +0530

    Update admin.py

--- tg_bot/modules/admin.py
+++ tg_bot/modules/admin.py
@@ -276,7 +276,8 @@ def invite(bot: Bot, update: Update):
         bot_member = chat.get_member(bot.id)
         if bot_member.can_invite_users:
             invitelink = bot.exportChatInviteLink(chat.id)
-            msg.reply_text("Invite-Link of <b>{}</b>\n\n{}".format(chat.title, invitelink)), parse_mode=ParseMode.HTML, disable_web_page_preview=True)
+            msg.reply_text("Invite-Link of <b>{}</b>\n\n{}".format(chat.title, invitelink)), parse_mode=ParseMode.MARKDOWN,
+                                                                                             disable_web_page_preview=True)
         else:
             msg.reply_text(
                 "I don't have access to the invite link, try changing my permissions!"
