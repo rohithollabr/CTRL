a446d3b68a20646f89ec351b9588c2f1b445766d
Singh <avatarx0tic@gmail.com>
2020-12-02 20:28:29 +0530

    [OKAY]

--- tg_bot/modules/getlink.py
+++ tg_bot/modules/getlink.py
@@ -1,6 +1,6 @@
 from typing import List
 
-from telegram import Update, Bot, Chat, Message, User
+from telegram import Update, Bot, Chat, Message, User, TelegramError
 from telegram.error import BadRequest
 from telegram.ext import CommandHandler, Filters
 from telegram.ext.dispatcher import run_async
@@ -12,17 +12,27 @@ from tg_bot import dispatcher
 @run_async
 @bot_admin
 def getlink(bot: Bot, update: Update, args: List[int]):
+    message = update.effective_message
     if args:
-        chat_id = int(args[0])
+        pattern = re.compile(r'-\d+')
     else:
-        update.effective_message.reply_text("You don't seem to be referring to a chat")
-    chat = bot.get_chat(chat_id)
-    bot_member = chat.get_member(bot.id)
-    if bot_member.can_invite_users:
-        invitelink = bot.exportChatInviteLink(chat_id)
-        update.effective_message.reply_text(invitelink)
-    else:
-        update.effective_message.reply_text("I don't have access to the invite link!")
+        message.reply_text("You don't seem to be referring to any chats.")
+    links = "Invite link(s):\n"
+    for chat_id in pattern.findall(message.text):
+        try:
+            chat = bot.getChat(chat_id)
+            bot_member = chat.get_member(bot.id)
+            if bot_member.can_invite_users:
+                invitelink = bot.exportChatInviteLink(chat_id)
+                links += str(chat_id) + ":\n" + invitelink + "\n"
+            else:
+                links += str(chat_id) + ":\nI don't have access to the invite link." + "\n"
+        except BadRequest as excp:
+                links += str(chat_id) + ":\n" + excp.message + "\n"
+        except TelegramError as excp:
+                links += str(chat_id) + ":\n" + excp.message + "\n"
+
+    message.reply_text(links)
 
 GETLINK_HANDLER = CommandHandler("getlink", getlink, pass_args=True, filters=CustomFilters.sudo_filter)
 
